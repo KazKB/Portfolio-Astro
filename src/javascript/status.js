474023690c309bd0fe626bafa4d57c5725870f55
KazKB <kaleelboston@gmail.com>
2022-12-20 20:52:49 +0000

    commit

--- src/javascript/status.js
+++ src/javascript/status.js
@@ -6,6 +6,8 @@ ageText.innerHTML = "Level: " + Math.floor(myAge/(31557600000));
 // The Tech Stack Interactivity
 const languageInfo = document.querySelectorAll(".description");
 const beginner = document.querySelectorAll(".beginner");
+const intermidiate = document.querySelectorAll(".intermidiate");
+const advanced = document.querySelectorAll(".advanced");
 
 // function toggle(description) {
 //     if (description.style.display == "block")
@@ -26,6 +28,26 @@ function hideStackInfo() {
 
 for (let element = 0; element < beginner.length; element++) {
     beginner[element].addEventListener("click", function(){
+        if (languageInfo[element].style.display == "block")
+            languageInfo[element].style.display = "none";
+        else {
+            hideStackInfo();
+            languageInfo[element].style.display = "block";
+        }
+    });
+}
+for (let element = 0; element < intermidiate.length; element++) {
+    intermidiate[element].addEventListener("click", function(){
+        if (languageInfo[element].style.display == "block")
+            languageInfo[element].style.display = "none";
+        else {
+            hideStackInfo();
+            languageInfo[element].style.display = "block";
+        }
+    });
+}
+for (let element = 0; element < advanced.length; element++) {
+    advanced[element].addEventListener("click", function(){
         if (languageInfo[element].style.display == "block")
             languageInfo[element].style.display = "none";
         else {
